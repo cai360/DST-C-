ec3b457d1cd4ef08f5236083552049233cc1ebec
cai360 <archery0360@gmail.com>
2025-06-04 11:53:33 +0800

    first commit

---
+++ Stack_implement_Linked_List/Stack_implement_Linked_List/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Stack_implement_Linked_List
+//
+//  Created by cai on 2025/6/4.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
